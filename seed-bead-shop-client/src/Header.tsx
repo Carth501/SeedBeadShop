84aad7f2b248ec1b8ad39076ab1495a12e735fb7
carth501 <carth501@gmail.com>
2025-02-06 17:25:14 -0500

    Migrate Header component to Tailwind CSS classes

--- seed-bead-shop-client/src/Header.tsx
+++ seed-bead-shop-client/src/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './Header.css';
 
 const Header: React.FC = () => {
 	return (
-		<header className="header">
-			<div className="header-content background">
-				<div className="logo-placeholder">Logo</div>
-				<div className="contact-info">
+		<header
+			className="align-middle border-b-solid border-b-2 
+		border-b-gray-300 w-[100vw] fixed top-0 left-0 z-1000"
+		>
+			<div className="background flex justify-between p-4">
+				<div className="text-2xl font-bold">Logo</div>
+				<div className="text-base">
 					info@example.com
 					<br />
 					+1 234 555 8902

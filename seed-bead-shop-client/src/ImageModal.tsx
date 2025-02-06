273b006b02f465b8eab0beae22aa64fb8a84d11e
carth501 <carth501@gmail.com>
2025-02-06 14:47:32 -0500

    Add navigation buttons with SVG icons to image modal

--- seed-bead-shop-client/src/ImageModal.tsx
+++ seed-bead-shop-client/src/ImageModal.tsx
@@ -25,10 +25,32 @@ const ImageModal: React.FC<ImageModalProps> = ({ images, isOpen, onClose }) => {
 			<div className="modal-content" onClick={(e) => e.stopPropagation()}>
 				<img src={images[currentIndex]} alt="Product" className="modal-image" />
 				<button onClick={handlePrevious} className="modal-button">
-					Previous
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="size-6"
+					>
+						<path
+							fillRule="evenodd"
+							d="M7.72 11.47a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 0 1-1.06 1.06l-7.5-7.5Z"
+							clipRule="evenodd"
+						/>
+					</svg>
 				</button>
 				<button onClick={handleNext} className="modal-button">
-					Next
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						className="size-6"
+					>
+						<path
+							fillRule="evenodd"
+							d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+							clipRule="evenodd"
+						/>
+					</svg>
 				</button>
 			</div>
 		</div>

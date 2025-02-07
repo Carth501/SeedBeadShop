4eb8bffa63d5a9a7844ea3efb7a6001fa64d3b33
carth501 <carth501@gmail.com>
2025-02-07 16:20:08 -0500

    Improve Header logo styling with Tailwind flex alignment

--- seed-bead-shop-client/src/Header.tsx
+++ seed-bead-shop-client/src/Header.tsx
@@ -9,10 +9,12 @@ const Header: React.FC = () => {
 			<div className="background flex justify-between p-4">
 				<div
 					className="text-2xl font-bold h-[2em] p-[1.25em] 
-				hover:drop-shadow-[0_0_2em_#646cffaa]"
+				align-middle hover:drop-shadow-[0_0_2em_#646cffaa] flex
+				items-center justify-center"
 				>
 					Logo
 				</div>
+
 				<div className="text-base">
 					info@example.com
 					<br />

e70037addd32fedd56f97fd2b6980c5b1fc26b88
carth501 <carth501@gmail.com>
2025-02-11 11:30:23 -0500

    Improve ProductCard layout and button positioning

--- seed-bead-shop-client/src/ProductCard.tsx
+++ seed-bead-shop-client/src/ProductCard.tsx
@@ -48,23 +48,28 @@ const ProductCard: React.FC<ProductCardProps> = ({
 		<div
 			className={`border border-gray-300 rounded-lg p-4 w-[200px] transition-all duration-300
 			shadow-md flex flex-col items-stretch shrink-0 background hover:scale-105 hover:shadow-lg
+			justify-between
 			${!isModalOpen ? 'hover' : ''}`}
 		>
-			{imageSrc && (
-				<img
-					src={imageSrc}
-					alt={label}
-					className="w-full h-auto aspect-square object-cover rounded hover:scale-105 
-					transition-transform duration-300"
-					onClick={() => onImageClick(id)}
-				/>
-			)}
-			<h2 className="text-left m-0">{label}</h2>
-			<p className="text-left m-0">{description}</p>
-			<p className="text-left m-0">{price}</p>
-			<p className="text-left m-0">{inStock ? 'In Stock' : 'Out of Stock'}</p>
-			<ProductButton label="One-Click Buy" onClick={() => console.log('One-Click Buy')} />
-			<ProductButton label="Add to Cart" onClick={() => console.log('Add to Cart')} />
+			<div>
+				{imageSrc && (
+					<img
+						src={imageSrc}
+						alt={label}
+						className="w-full h-auto aspect-square object-cover rounded hover:scale-105 
+						transition-transform duration-300"
+						onClick={() => onImageClick(id)}
+					/>
+				)}
+				<h2 className="text-left m-0">{label}</h2>
+				<p className="text-left m-0">{description}</p>
+				<p className="text-left m-0">{price}</p>
+				<p className="text-left m-0">{inStock ? 'In Stock' : 'Out of Stock'}</p>
+			</div>
+			<div className="flex flex-col items-stretch">
+				<ProductButton label="One-Click Buy" onClick={() => console.log('One-Click Buy')} />
+				<ProductButton label="Add to Cart" onClick={() => console.log('Add to Cart')} />
+			</div>
 			<ImageModal images={images} isOpen={isModalOpen} onClose={handleCloseModal} />
 		</div>
 	);

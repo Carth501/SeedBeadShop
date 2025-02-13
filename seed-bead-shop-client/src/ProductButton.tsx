7c701aca46e2d03729c361fcdb8da3cc349a84b6
carth501 <carth501@gmail.com>
2025-02-13 14:14:39 -0500

    Refactor ProductButton styles for improved hover and active effects

--- seed-bead-shop-client/src/ProductButton.tsx
+++ seed-bead-shop-client/src/ProductButton.tsx
@@ -10,14 +10,14 @@ const ProductButton: React.FC<ProductButtonProps> = ({ label, onClick }) => {
 		<button
 			className="product-button border border-transparent 
 			px-4 py-2 text-base font-bold cursor-pointer
-			bg-gradient-to-tr from-rose-300 to-rose-200
-			hover:from-rose-200 hover:to-rose-400
+			bg-gradient-to-tr from-rose-400 to-rose-300
+			hover:from-rose-300 hover:to-rose-500
 			transition duration-250 m-1 text-white
 			rounded-2xl shadow-md text-shadow-lg 
-			hover:scale-110 hover:border-rose-500
-			hover:shadow-rose-200 hover:shadow-lg
-			active:shadow-rose-300 active:shadow-xl
-			active:from-rose-100 active:to-rose-500"
+			hover:scale-110 hover:border-rose-600
+			hover:shadow-rose-300 hover:shadow-lg
+			active:shadow-rose-400 active:shadow-xl
+			active:from-rose-200 active:to-rose-600"
 			onClick={onClick}
 		>
 			{label}

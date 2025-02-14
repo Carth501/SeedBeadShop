1f0a11182db70bfed2d77fc8d3ca3c2889e7777c
carth501 <carth501@gmail.com>
2025-02-14 18:30:22 -0500

    Add "Shop" button to ShowcasePanel for improved user interaction

--- seed-bead-shop-client/src/components/ShowcasePanel.tsx
+++ seed-bead-shop-client/src/components/ShowcasePanel.tsx
@@ -12,6 +12,9 @@ const ShowcasePanel: React.FC<ShowcasePanelProps> = ({ imageUrl, opacity }) => {
 			style={{ opacity }}
 		>
 			<img src={imageUrl} alt="Panel" className="w-full h-full object-cover" />
+			<button className="absolute bottom-10 bg-white text-black px-4 py-2 rounded">
+				Shop
+			</button>
 		</div>
 	);
 };

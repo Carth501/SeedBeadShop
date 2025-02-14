f361360d0e501d54289c20fedc5cc9d586bcc4a5
carth501 <carth501@gmail.com>
2025-02-14 18:37:39 -0500

    Enhance ShowcasePanel with overflow control and improved Shop button styles

--- seed-bead-shop-client/src/components/ShowcasePanel.tsx
+++ seed-bead-shop-client/src/components/ShowcasePanel.tsx
@@ -8,11 +8,24 @@ interface ShowcasePanelProps {
 const ShowcasePanel: React.FC<ShowcasePanelProps> = ({ imageUrl, opacity }) => {
 	return (
 		<div
-			className="absolute inset-0 flex justify-center items-center transition-opacity duration-1000"
+			className="absolute inset-0 flex justify-center items-center transition-opacity duration-1000 overflow-x-hidden"
 			style={{ opacity }}
 		>
-			<img src={imageUrl} alt="Panel" className="w-full h-full object-cover" />
-			<button className="absolute bottom-10 bg-white text-black px-4 py-2 rounded">
+			<img src={imageUrl} alt="Panel" className="object-cover" />
+			<button
+				className="absolute bottom-10 
+			product-button border border-transparent 
+			px-4 py-2 text-base font-bold cursor-pointer
+			bg-gradient-to-tr from-rose-400 to-rose-300
+			hover:from-rose-300 hover:to-rose-500
+			transition duration-250 m-1 text-white
+			rounded-2xl shadow-md text-shadow-lg 
+			hover:scale-110 hover:border-rose-600
+			hover:shadow-rose-300 hover:shadow-lg
+			active:shadow-rose-400 active:shadow-xl
+			active:from-rose-200 active:to-rose-600
+			hover:border-none"
+			>
 				Shop
 			</button>
 		</div>

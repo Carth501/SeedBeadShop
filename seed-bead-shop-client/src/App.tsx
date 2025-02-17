e727b48fa319483747aedd73077da9257d2babae
carth501 <carth501@gmail.com>
2025-02-17 08:44:31 -0500

    Remove blossom animation, wrap body in container

--- seed-bead-shop-client/src/App.tsx
+++ seed-bead-shop-client/src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Blossoms from './blossoms/blossoms';
 import ImagePanelCycle from './components/ShowcasePanelCycle';
 import Header from './Header';
 import ImageGallery from './ImageGallery';
@@ -46,31 +45,34 @@ function App() {
 	return (
 		<>
 			<Header />
-			<ImagePanelCycle />
-			<div className="h-40 -z-1000">
-				<ImageGallery images={products[2]?.images || []} />
+			<div>
+				<ImagePanelCycle />
+				<div className="h-40 -z-1000">
+					<ImageGallery images={products[2]?.images || []} />
+				</div>
+				<div
+					className="product-row flex justify-center overflow-x-auto overflow-y-visible 
+				read-the-docsp-4 gap-4 w-screen p-4"
+				>
+					{products.map((product, index) => (
+						<ProductCard
+							key={index}
+							id={index}
+							price={product.price}
+							label={product.label}
+							description={product.description}
+							inStock={product.inStock}
+							images={product.images}
+							onImageClick={handleImageClick}
+						/>
+					))}
+				</div>
+				<ImageModal
+					images={currentImages}
+					isOpen={isModalOpen}
+					onClose={handleCloseModal}
+				/>
 			</div>
-			<div
-				className="product-row flex justify-center overflow-x-auto overflow-y-visible 
-			read-the-docsp-4 gap-4 w-screen p-4"
-			>
-				{products.map((product, index) => (
-					<ProductCard
-						key={index}
-						id={index}
-						price={product.price}
-						label={product.label}
-						description={product.description}
-						inStock={product.inStock}
-						images={product.images}
-						onImageClick={handleImageClick}
-					/>
-				))}
-			</div>
-			<div className="absolute top-0 left-0 w-[95%] h-screen -z-1">
-				<Blossoms />
-			</div>
-			<ImageModal images={currentImages} isOpen={isModalOpen} onClose={handleCloseModal} />
 			<div className="fixed inset-0 bg-white opacity-50 -z-1000"></div>
 		</>
 	);

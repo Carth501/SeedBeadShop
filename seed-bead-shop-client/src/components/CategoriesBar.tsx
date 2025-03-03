2f08b5344ee870de053a0afba389cdc1df391a5c
carth501 <carth501@gmail.com>
2025-03-02 20:58:58 -0500

    Enhance button styles in CategoriesBar for improved UI/UX

--- seed-bead-shop-client/src/components/CategoriesBar.tsx
+++ seed-bead-shop-client/src/components/CategoriesBar.tsx
@@ -11,19 +11,49 @@ const CategoriesBar: React.FC = () => {
 	return (
 		<div className="categories-bar flex justify-center gap-4 p-4">
 			<button
-				className="bg-blue-500 text-white py-2 px-4 rounded"
+				className="border border-transparent 
+			px-4 py-2 text-base font-bold cursor-pointer
+			bg-gradient-to-tr from-rose-400 to-rose-300
+			hover:from-rose-300 hover:to-rose-500
+			transition duration-250 m-1 text-white
+			rounded-2xl shadow-md text-shadow-lg 
+			hover:scale-110 hover:border-rose-600
+			hover:shadow-rose-300 hover:shadow-lg
+			active:shadow-rose-400 active:shadow-xl
+			active:from-rose-200 active:to-rose-600
+			hover:border-none"
 				onClick={() => handleCategorySelect('earring')}
 			>
 				Earrings
 			</button>
 			<button
-				className="bg-blue-500 text-white py-2 px-4 rounded"
+				className="border border-transparent 
+			px-4 py-2 text-base font-bold cursor-pointer
+			bg-gradient-to-tr from-rose-400 to-rose-300
+			hover:from-rose-300 hover:to-rose-500
+			transition duration-250 m-1 text-white
+			rounded-2xl shadow-md text-shadow-lg 
+			hover:scale-110 hover:border-rose-600
+			hover:shadow-rose-300 hover:shadow-lg
+			active:shadow-rose-400 active:shadow-xl
+			active:from-rose-200 active:to-rose-600
+			hover:border-none"
 				onClick={() => handleCategorySelect('necklace')}
 			>
 				Necklaces
 			</button>
 			<button
-				className="bg-blue-500 text-white py-2 px-4 rounded"
+				className="border border-transparent 
+			px-4 py-2 text-base font-bold cursor-pointer
+			bg-gradient-to-tr from-rose-400 to-rose-300
+			hover:from-rose-300 hover:to-rose-500
+			transition duration-250 m-1 text-white
+			rounded-2xl shadow-md text-shadow-lg 
+			hover:scale-110 hover:border-rose-600
+			hover:shadow-rose-300 hover:shadow-lg
+			active:shadow-rose-400 active:shadow-xl
+			active:from-rose-200 active:to-rose-600
+			hover:border-none"
 				onClick={() => handleCategorySelect('bracelet')}
 			>
 				Bracelets
